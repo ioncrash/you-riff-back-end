b7e1dec1216c75b13a2261c3232cf741b6ba98de
ioncrash <ioncrash@gmail.com>
2017-01-15 16:13:32 -0500

    added defs into all serializers for other assets
    now properly renders on front end

--- app/serializers/riff_serializer.rb
+++ app/serializers/riff_serializer.rb
@@ -1,7 +1,15 @@
 class RiffSerializer < ActiveModel::Serializer
-  attributes :id, :text, :stamp, :flagged, :user_id, :video_id, :editable
+  attributes :id, :text, :stamp, :flagged, :user, :video, :editable
 
   def editable
     scope == object.user
+  end
+
+  def video
+    object.video.id
+  end
+
+  def user
+    object.user.id
   end
 end
--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,3 +1,7 @@
 class VideoSerializer < ActiveModel::Serializer
   attributes :id, :ytid, :users, :riffs
+
+  def riffs
+    object.riffs.pluck(:id)
+  end
 end
